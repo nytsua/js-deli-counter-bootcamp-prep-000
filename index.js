641d02315cf560c24a710560270d8364da706e0a
Austyn Trace <atrace@gmail.com>
2017-09-04 01:24:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return 'There is nobody waiting to be served!';
+  }
   return katzDeliLine.shift();
 }
 
