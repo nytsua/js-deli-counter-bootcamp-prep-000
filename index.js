2d552a47a34c500390a62182ef6a33f5264d95a9
Austyn Trace <atrace@gmail.com>
2017-09-04 01:18:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function takeANumber(name){
 function nowServing(){
   return katzDeli.shift();
 }
+
+function currentLine(){
+  if(katzDeli.length === 0){
+    return 'The line is currently empty.'
+  } else {
+    
+  }
+  var toReturn = 'The line is currently: 1. katzDeliLine[0],';
+  for(let i =1;i<toReturn.length-1; i++){
+    toReturn += `${i+1}.  `
+  }
+}
