9293dc9d2807c08869822c1ef112eef168e68d88
Austyn Trace <atrace@gmail.com>
2017-09-04 01:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeli.push(name);
-  return katzDeli.length;
+  katzDeliLine.push(name);
+  return katzDeliLine.length;
 }
 
-function nowServing(){
-  return katzDeli.shift();
+function nowServing(katzDeliLine){
+  return katzDeliLine.shift();
 }
 
-function currentLine(){
-  if(katzDeli.length === 0){
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
     return 'The line is currently empty.'
   } else {
-
+    var toReturn = 'The line is currently: 1. katzDeliLine[0]';
+    for(let i =1;i<toReturn.length-1; i++){
+      toReturn += `, ${i+1}. katzDeliLine[i]`
+    }
+    return toReturn;
   }
-  var toReturn = 'The line is currently: 1. katzDeliLine[0]';
-  for(let i =1;i<toReturn.length-1; i++){
-    toReturn += `, ${i+1}. katzDeliLine[i]`
-  }
-  return toReturn;
 }
