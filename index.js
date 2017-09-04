b69063118bb466c89fb333eda54219bfec58d5c4
Austyn Trace <atrace@gmail.com>
2017-09-04 01:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function currentLine(){
   if(katzDeli.length === 0){
     return 'The line is currently empty.'
   } else {
-    
+
   }
-  var toReturn = 'The line is currently: 1. katzDeliLine[0],';
+  var toReturn = 'The line is currently: 1. katzDeliLine[0]';
   for(let i =1;i<toReturn.length-1; i++){
-    toReturn += `${i+1}.  `
+    toReturn += `, ${i+1}. katzDeliLine[i]`
   }
 }
